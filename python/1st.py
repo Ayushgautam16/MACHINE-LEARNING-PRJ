65fdddf44edc7a28de337bf7835bcbcbad6832f1
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-11-21 10:43:49 +0530

    1

---
+++ python/1st.py
@@ -0,0 +1,96 @@
+import random
+# int is mutable
+a = 3
+print(a)
+a = random.choice
+print(a)
+
+a = 3.14
+print(a)
+
+mylist1 = [1, 2, 3, 4, 5]
+
+# list is mutable
+
+mylist1[0] = 100
+print(mylist1)
+
+mylist2 = mylist1
+mylist2[1] = 200
+print(mylist2)
+
+mylist3 = mylist1.copy()
+print(mylist3) 
+# concption 
+
+p1 = [1,2,3,4]
+p2 = p1
+p2 = [1,2,3,4]
+p2[0] = 100
+
+print(p2)
+print(p1)
+
+h1 = [1,2,3,4]
+h2 = h1[:]
+print(h1)
+
+m = [1,2,3,4]
+n = [1,2,3,4]
+
+print(m)
+print(n)
+print (m == n)
+print (m is n)
+
+3.14
+[100, 2, 3, 4, 5]
+[100, 200, 3, 4, 5]
+[100, 200, 3, 4, 5]
+[100, 2, 3, 4]
+[1, 2, 3, 4]
+[1, 2, 3, 4]
+[1, 2, 3, 4]
+[1, 2, 3, 4]
+True
+False
+PS C:\Users\ayush\OneDrive\Desktop\MACHINE LEARNING PRJ> python
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> x= 2
+>>> y = 3
+>>> z = 4
+>>> x+y
+5
+>>> (x+y)+z
+9
+>>> 40.0
+40.0
+>>> "hitesh" + "chai"
+'hiteshchai'
+>>> x,y,z 
+(2, 3, 4)
+>>> (2,3,4)
+(2, 3, 4)
+>>> y%3
+0
+>>> 100**2
+10000
+>>> 2**1000
+10715086071862673209484250490600018105614048117055336074437503883703510511249361224931983788156958581275946729175531468251871452856923140435984577574698574803934567774824230985421074605062371141877954182153046474983581941267398767559165543946077062914571196477686542167660429831652624386837205668069376
+>>> result = 1/3.3
+>>> result
+0.30303030303030304
+>>> 1<2
+True
+>>> 2>4
+False
+>>> 5.0 ==5.0
+True
+>>> 5.0!=5.0
+False
+>>> x,y,z
+(2, 3, 4)
+>>> x<y<z
+True
+>>>
---
+++ python/datatypes.md
@@ -0,0 +1,4 @@
+#Object types/ data types
+
+
+import 
